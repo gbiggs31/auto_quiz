b624ff63e774b2c94f952318f63db44deea8bf83
gbiggs31 <georgebiggs1@hotmail.com>
2020-07-12 12:38:21 +0100

    version 1.0, created initial scripts

---
+++ auto_quiz_app.py
@@ -0,0 +1,13 @@
+# goal is to produce a random quiz question
+# test if streamlit can help make this user interactive
+
+
+
+# Import packages
+
+
+
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
